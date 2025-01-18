efbc278a04de993ee59ec1bf6be1541a80724fdc
Muhammad Ilham Surya Lesmana <ilhamrampage.mr@gmail.com>
2025-01-18 20:17:10 +0700

    Done submitted
    The assignment is complete and submitted to Freecode camp, and was considered passed

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-const inputText = document.getElementById("text-input").value;
+const inputText = document.getElementById("text-input");
 const resultText = document.getElementById("result");
-const checkBtn  = document.getElementById("check-btn");
+const checkBtn = document.getElementById("check-btn");
 
-let palindrome = false
 
-function checkPalindrome(){
-    if(inputText.length=1){
-         palindrome = true
-    }
+// regex for clearing other than alphanumeric
+function clearString(str){
+    const regex= /[\s\W_]/g;
+    return str.replace(regex,"");
 }
-function checkText(){
-    
-    if(inputText.value === ""){
-        throw alert("Please input");
-    }else{
-     resultText.innerText = inputText + " is " + palindrome;
+// checking palindrome
+function checkPalindrome() {
+    // clearing non alphanumeric
+    let text = clearString(inputText.value.toLowerCase());
+    // single char mean palindrome
+    if (text.length === 1) {
+        return true;
     }
+    // checking char each iteration until the middle of string
+    for(let i = 0; i<Math.floor(text.length/2);i++){
+        if(text[i]!==text[text.length-1-i]){
+           return false;
+        }
+    };
+    // if passed means palindrome
+    return true;
 };
+
+// checking text with checkPalindrome
+function checkText() {
+    if (inputText.value === "") {
+        alert("Please input a value");
+        return;
+    }
+    const palindrome = checkPalindrome(inputText)
+    if (palindrome) {
+        resultText.innerText = inputText.value + " is a Palindrome";
+    } else {
+        resultText.innerText = inputText.value + " is not a palindrome";
+    }
+    console.log(palindrome);
+    resultText.classList.remove("hide");
+};
+
+checkBtn.addEventListener("click",checkText)
